dd603e644268c038518d7fd641611a6e50ecb253
Neil Shannon <neil.shannon@cognizant.com>
2018-12-18 18:08:57 -0600

    buildpack is picky

--- bin/createFunction.sh
+++ bin/createFunction.sh
@@ -1,4 +1,4 @@
-pfs function create randomwords \
+pfs function create randomwords2 \
 --git-repo https://github.com/neilshannon/node-pfs-demo \
---image $REGISTRY/$REGISTRY_USER/randomwords \
+--image $REGISTRY/$REGISTRY_USER/randomwords2 \
 --verbose
